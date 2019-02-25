72d58c2cfa45e737cfb667fb5b84296d3b49ad4f
zw <zw@zwdemac.local>
2019-02-25 15:25:07 +0800

    save work

--- CodeLib/MyLib/ED_PageView/ED_PageView.h
+++ CodeLib/MyLib/ED_PageView/ED_PageView.h
@@ -9,58 +9,11 @@
 #import <UIKit/UIKit.h>
 
 
-//@class ED_PageView;
-//@protocol ED_PageViewHandleDelegate <NSObject>
-//
-//
-//@required
-//
-//// 零界点
-//- (CGFloat)zeroPointWithPageView:(ED_PageView *)pageView;
-//
-////---------------------------------------------------------------------
-//- (UITableViewCell *)pageView:(ED_PageView *)pageView cellForRowAtIndexPath:(NSIndexPath *)indexPath viewTag:(NSInteger)tag;
-//
-//
-//- (NSInteger)pageView:(ED_PageView *)pageView numOfSectionWithViewTag:(NSInteger)tag;
-//
-//
-//- (NSInteger)pageView:(ED_PageView *)pageView numOfRowInSection:(NSInteger)section viewTag:(NSInteger)tag;
-//
-//- (void)pageView:(ED_PageView *)pageView didSelectRowAtIndexPath:(NSIndexPath *)indexPath viewTag:(NSInteger)tag ;
-//
-////----------------------------------------------------------------------
-//
-//- (NSArray<NSString *> *)titleArrayWithPageView:(ED_PageView *)pageView;
-//
-//
-//
-//
-//@end
 
-//@class ED_PageContextManager;
+
+
 @interface ED_PageView : UIView
 
-//- (instancetype)initWithManager:(ED_PageContextManager *)manager;
-
-
-
-
-//@property (nonatomic , weak) id<ED_PageViewHandleDelegate>delegate;
-//
-//
-//- (instancetype)initWithDelegate:(id<ED_PageViewHandleDelegate>)delegate;
-//
-//
-//@property (nonatomic , assign) CGFloat titlHight;
-//
-//
-//- (void)reloadAll;
-//
-//- (void)reloadWithTag:(NSInteger)tag;
-//
-//
-//- (UITableViewCell *)dequeueReusableCellWithIdentifier:(NSString *)identifier viewTag:(NSInteger)tag;
 
 
 @end
